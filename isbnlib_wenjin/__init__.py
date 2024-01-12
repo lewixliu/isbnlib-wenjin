2f7216b4dfabf0e2d63bea02dc9ef3aceb8d229d
Lewix <lewix@ustc.edu>
2024-01-13 00:35:44 +0800

    isbnlib plugin for Wenjin (find.nlc.cn)

---
+++ isbnlib_wenjin/__init__.py
@@ -0,0 +1,10 @@
+from ._wenjin import query
+
+__version__ = '0.0.1'
+
+__plugin_author__ = 'Lewix'
+__plugin_description__ = 'Metadata for books from Wenjin of NLC'
+__plugin_homepage__ = 'github.com/lewixliu/isbnlib-wenjin'
+__plugin_service_language__ = 'zh'
+__plugin_service_name__ = 'wenjin'
+__plugin_type__ = 'metadata'
